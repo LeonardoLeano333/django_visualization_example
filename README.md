22bb47134ee77b64640f0d574d6df212bb36db32
unknown <leodrl9@gmail.com>
2020-03-01 21:23:26 -0300

    add test coverage

--- README.md
+++ README.md
@@ -24,8 +24,8 @@ Look for "jupter_treatment > data_presentation.ipynb", in the jupter platform.
 ## running web application for the dash boards
 - running: 
 ```
-python -m web_visualization_only/manage.py runserver
+python web_visualization_only/manage.py runserver
 ```
-- look at broser: 
+- look at browser: 
 http://localhost:8000
 
--- web_visualization_only/simple_visualization/tests.py
+++ web_visualization_only/simple_visualization/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from simple_visualization.views import Graph
+from django.test import Client
+import logging
 
 # Create your tests here.
+class TestSimpleVisualizationGraph(TestCase):
+    def test_template(self):
+        client = Client()
+        response = client.get('')
+        logging.error(f'got it {response}')
